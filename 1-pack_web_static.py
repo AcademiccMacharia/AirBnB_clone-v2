a1bf5e7f23787ea860bdac60fea5ce0eaa539ab4
AcademiccMacharia <machariab19@gmail.com>
2023-08-07 09:39:31 +0300

    fixing errors

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,19 +1,22 @@
 #!/usr/bin/python3
 """ Function that compress a folder """
-from datetime import datetime
 from fabric.api import local
+from datetime import datetime
 import os
 
 
 def do_pack():
-    try:
-        if not os.path.exists("versions"):
-            local('mkdir versions')
-        t = datetime.now()
-        f = "%Y%m%d%H%M%S"
-        archive_path = 'versions/web_static_{}.tgz'.format(t.strftime(f))
-        local('tar -cvzf {} web_static'.format(archive_path))
-        return archive_path
-    except:
-        return None
+    """Generates a .tgz archive from the contents of the web_static folder"""
 
+    if not os.path.exists("versions"):
+        os.makedirs("versions")
+
+    timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+    archive_name = "web_static_{}.tgz".format(timestamp)
+
+    try:
+        local("tar -czvf versions/{} web_static".format(archive_name))
+        return "versions/{}".format(archive_name)
+    except Exception as e:
+        print("Error creating archive:", e)
+        return None
