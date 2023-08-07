ec287d204e99b44940bc1dc8ac5f31f5cdb8fa49
AcademiccMacharia <machariab19@gmail.com>
2023-08-07 11:52:56 +0300

    fixing error

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,32 +1,40 @@
 #!/usr/bin/python3
 """Distributes an archive to my web servers"""
+from datetime import datetime
 from fabric.api import *
 import os
+import shlex
 
 env.hosts = ['100.26.228.117', '107.22.146.104']
 env.user = "ubuntu"
 
 
 def do_deploy(archive_path):
-    """Distributes an archive to the web servers and deploys it."""
-
+    """ Deploys """
     if not os.path.exists(archive_path):
         return False
     try:
+        name = archive_path.replace('/', ' ')
+        name = shlex.split(name)
+        name = name[-1]
+
+        wname = name.replace('.', ' ')
+        wname = shlex.split(wname)
+        wname = wname[0]
+
+        releases_path = "/data/web_static/releases/{}/".format(wname)
+        tmp_path = "/tmp/{}".format(name)
+
         put(archive_path, "/tmp/")
-
-        archive_filename = os.path.basename(archive_path)
-        folder_name = "/data/web_static/releases/{}".format(
-            archive_filename.replace(".tgz", "").replace(".tar.gz", ""))
-        run("mkdir -p {}".format(folder_name))
-        run("tar -xzf /tmp/{} -C {}".format(archive_filename, folder_name))
-        run("rm /tmp/{}".format(archive_filename))
+        run("mkdir -p {}".format(releases_path))
+        run("tar -xzf {} -C {}".format(tmp_path, releases_path))
+        run("rm {}".format(tmp_path))
+        run("mv {}web_static/* {}".format(releases_path, releases_path))
+        run("rm -rf {}web_static".format(releases_path))
         run("rm -rf /data/web_static/current")
-        run("ln -s {} /data/web_static/current".format(folder_name))
-
-        print("New Version deployed!")
+        run("ln -s {} /data/web_static/current".format(releases_path))
+        print("New version deployed!")
         return True
-
     except Exception as e:
         print("Error deploying:", e)
         return False
